850ea0ec6277cd0897154460b4bf15f65a6c4992
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-05 18:26:11 +0300

    [3] urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,25 @@
 
 package main
 
-func main() {
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+)
 
+func main() {
+    for _, arg := range os.Args[1:] {
+        resp, err := http.Get(arg)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer resp.Body.Close()
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(string(body))
+    }
 }
