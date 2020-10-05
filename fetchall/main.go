024f5704b1223a99ac67aa2204e397c3480ef89f
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-05 19:20:46 +0300

    [4] fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,35 @@
 
 package main
 
-func main() {
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "sync"
+    "time"
+)
 
+func main() {
+    var wg sync.WaitGroup
+    totalStart := time.Now()
+
+    for _, url := range os.Args[1:] {
+        wg.Add(1)
+        go func(url string, wg *sync.WaitGroup) {
+            defer wg.Done()
+            start := time.Now()
+            resp, err := http.Get(url)
+            duration := time.Since(start)
+            if err != nil {
+                fmt.Println(err)
+                return
+            }
+            body, err := ioutil.ReadAll(resp.Body)
+            fmt.Printf("%s\t%d\t%s\n", duration, len(body), url)
+        }(url, &wg)
+    }
+    wg.Wait()
+    totalDur := time.Since(totalStart)
+    fmt.Printf("%s elapsed\n", totalDur)
 }
