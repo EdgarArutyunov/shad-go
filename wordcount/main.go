dab9103171f7ee61e97ea4b2406543658def3619
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-05 18:14:14 +0300

    [2] Wordcount.go

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
 
+func main() {
+    score := make(map[string]int64)
+    for _, arg := range os.Args[1:] {
+        file, err := os.Open(arg)
+        if err != nil {
+            log.Fatal(err)
+        }
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+            score[scanner.Text()]++
+        }
+        file.Close()
+    }
+
+    for key, val := range score {
+        if val >= 2 {
+            fmt.Printf("%d\t%s\n", val, key)
+        }
+    }
 }
