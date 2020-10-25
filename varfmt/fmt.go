0cdcf531e2d3660fd9548edfb6b0fadcd7af23ae
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-26 01:31:15 +0300

    [2.4] varfmt

--- varfmt/fmt.go
+++ varfmt/fmt.go
@@ -2,6 +2,47 @@
 
 package varfmt
 
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
+
+// Sprintf ...
 func Sprintf(format string, args ...interface{}) string {
-	return ""
+	var buf, curArg bytes.Buffer
+
+	const Nul = 0
+	const Open = 1
+
+	st := Nul
+	argNum := 0
+	for i := 0; i < len(format); {
+		r, sz := utf8.DecodeRuneInString(format[i:])
+		i += sz
+		switch st {
+		case Nul:
+			if r == '{' {
+				st = Open
+			} else {
+				buf.WriteRune(r)
+			}
+		case Open:
+			if r == '}' {
+				if len(curArg.String()) != 0 {
+					arg, _ := strconv.Atoi(curArg.String())
+					curArg.Reset()
+					buf.WriteString(fmt.Sprint(args[arg]))
+				} else {
+					buf.WriteString(fmt.Sprint(args[argNum]))
+				}
+				argNum++
+				st = Nul
+			} else {
+				curArg.WriteRune(r)
+			}
+		}
+	}
+	return buf.String()
 }
