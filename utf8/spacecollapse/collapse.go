c9eb571b8f495751727774a91f9d9a6243328890
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-27 00:14:52 +0300

    [3.3] utf8

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,26 @@
 
 package spacecollapse
 
+import (
+	"bytes"
+	"unicode"
+	"unicode/utf8"
+)
+
+// CollapseSpaces ...
 func CollapseSpaces(input string) string {
-	return ""
+	var buf bytes.Buffer
+	for i, lastSpace := 0, false; i < len(input); {
+		r, sz := utf8.DecodeRuneInString(input[i:])
+		i += sz
+
+		if !unicode.IsSpace(r) {
+			lastSpace = false
+			buf.WriteRune(r)
+		} else if !lastSpace {
+			lastSpace = true
+			buf.WriteRune(' ')
+		}
+	}
+	return buf.String()
 }
