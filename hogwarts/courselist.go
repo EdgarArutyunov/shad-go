503e860d31caed56fa98520bc8cbcec9dc9ec6e0
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-26 23:51:02 +0300

    [3.2] courselist

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -2,6 +2,35 @@
 
 package hogwarts
 
+// GetCourseList ...
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	res := make([]string, 0, len(prereqs))
+	used := make(map[string]bool)
+	done := make(map[string]bool)
+	var dfs func(cur string)
+	dfs = func(cur string) {
+		used[cur] = true
+		for _, next := range prereqs[cur] {
+			_, cicle := used[next]
+			_, done := done[next]
+			if done {
+				continue
+			}
+
+			if cicle {
+				panic("Cicle!")
+			}
+
+			dfs(next)
+		}
+		done[cur] = true
+		res = append(res, cur)
+	}
+
+	for key := range prereqs {
+		if _, ok := used[key]; !ok {
+			dfs(key)
+		}
+	}
+	return res
 }
