e4e821aae0d4a8e4de1364edacc29d32e4b177cd
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-10-31 23:59:30 +0300

    [4.3] add writer

--- externalsort/sort.go
+++ externalsort/sort.go
@@ -61,9 +61,30 @@ func NewReader(r io.Reader) LineReader {
 	}
 }
 
+// MyLineWriter ...
+type MyLineWriter struct {
+	w io.Writer
+}
+
+// Write ...
+func (mw *MyLineWriter) Write(l string) error {
+	buf := []byte(l)
+	buf = append(buf, '\n')
+	n, err := mw.w.Write(buf)
+	if err != nil {
+		return err
+	}
+	if n != len(buf) {
+		return fmt.Errorf("not all data")
+	}
+	return nil
+}
+
 // NewWriter ...
 func NewWriter(w io.Writer) LineWriter {
-	panic("implement me")
+	return &MyLineWriter{
+		w: w,
+	}
 }
 
 // Merge ...
