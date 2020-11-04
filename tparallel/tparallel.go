3220fb6cbb6be237142ade1bf624b7d085e99efd
Edgar Arutyunov <e.arutyunov@rt-solar.ru>
2020-11-05 01:16:27 +0300

    [5.1] tparallel

--- tparallel/tparallel.go
+++ tparallel/tparallel.go
@@ -2,17 +2,85 @@
 
 package tparallel
 
+// T ...
 type T struct {
+	chGo                chan struct{}
+	waitTokenFromParent chan struct{}
+	sendTokensToChild   []chan struct{}
+	waitEndOfChilds     []chan struct{}
+	sendEndToParent     chan struct{}
 }
 
+// Parallel ...
 func (t *T) Parallel() {
-	panic("implement me")
+	t.chGo <- struct{}{}
+	<-t.waitTokenFromParent
 }
 
+// Run ...
 func (t *T) Run(subtest func(t *T)) {
-	panic("implement me")
+
+	curEndID := len(t.waitEndOfChilds)
+	t.waitEndOfChilds = append(
+		t.waitEndOfChilds,
+		make(chan struct{}, 1),
+	)
+
+	t.sendTokensToChild = append(
+		t.sendTokensToChild,
+		make(chan struct{}, 1),
+	)
+
+	subT := &T{
+		chGo:                make(chan struct{}, 1),
+		waitTokenFromParent: t.sendTokensToChild[curEndID],
+		sendTokensToChild:   make([]chan struct{}, 0),
+		waitEndOfChilds:     make([]chan struct{}, 0),
+		sendEndToParent:     t.waitEndOfChilds[curEndID],
+	}
+
+	go func(subT *T, t *T) {
+		subtest(subT)
+		defer func() {
+			for _, send := range subT.sendTokensToChild {
+				send <- struct{}{}
+			}
+
+			for _, getEnd := range subT.waitEndOfChilds {
+				<-getEnd
+			}
+
+			subT.sendEndToParent <- struct{}{}
+		}()
+	}(subT, t)
+
+	select {
+	case <-t.waitEndOfChilds[curEndID]:
+		close(t.waitEndOfChilds[curEndID])
+
+	case <-subT.chGo:
+	}
 }
 
+// Run ...
 func Run(topTests []func(t *T)) {
-	panic("implement me")
+	t := &T{
+		chGo:                nil,
+		waitTokenFromParent: nil,
+		sendTokensToChild:   make([]chan struct{}, 0),
+		waitEndOfChilds:     make([]chan struct{}, 0),
+		sendEndToParent:     nil,
+	}
+
+	for _, tst := range topTests {
+		t.Run(tst)
+	}
+
+	for _, send := range t.sendTokensToChild {
+		send <- struct{}{}
+	}
+
+	for _, wait := range t.waitEndOfChilds {
+		<-wait
+	}
 }
